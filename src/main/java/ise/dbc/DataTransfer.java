02d1271ef8592bf3bf1759f86a9cfbf3b40343f5
bamboo <hias.fuchs@gmail.com>
2017-07-21 22:05:25 +0200

    ordered files

--- src/main/java/ise/dbc/DataTransfer.java
+++ src/main/java/ise/dbc/DataTransfer.java
@@ -1,31 +1,5 @@
 package ise.dbc;
 
-// -----------------------------------------------------------------------------
-// CLOBFileExample.java
-// -----------------------------------------------------------------------------
-
-/*
- * =============================================================================
- * Copyright (c) 1998-2011 Jeffrey M. Hunter. All rights reserved.
- *
- * All source code and material located at the Internet address of
- * http://www.idevelopment.info is the copyright of Jeffrey M. Hunter and
- * is protected under copyright laws of the United States. This source code may
- * not be hosted on any other site without my express, prior, written
- * permission. Application to host any of the material elsewhere can be made by
- * contacting me at jhunter@idevelopment.info.
- *
- * I have made every effort and taken great care in making sure that the source
- * code and other content included on my web site is technically accurate, but I
- * disclaim any and all responsibility for any loss, damage or destruction of
- * data or any other property which may arise from relying on it. I will in no
- * case be liable for any monetary damages arising from such loss, damage or
- * destruction.
- *
- * As with any code, ensure to test this code in a development environment
- * before attempting to run it in production.
- * =============================================================================
- */
 
 import java.sql.*;
 
@@ -73,14 +47,6 @@ public class DataTransfer {
     private static String VERSIONDBC = "A1_00_30";
 
 
-    /**
-     * Default constructor used to create this object. Responsible for setting
-     * this object's creation date, as well as incrementing the number instances
-     * of this object.
-     *
-     * @param args Array of string arguments passed in from the command-line.
-     * @throws java.io.IOException
-     */
     public DataTransfer(String PropertiesFile) throws IOException {
 
         inputTextFileName = PropertiesFile;
@@ -174,10 +140,10 @@ public class DataTransfer {
      *
      * @param args Array of string arguments passed in from the command-line.
      */
+
     public static void main(String[] args) throws IOException {
 
         DataTransfer.running = true;
-
 
         DataTransfer cLOBFileExample = null;
         System.out.println("DBANALYTIC Start");
---
+++ src/main/resources/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export ANALYSIS_CLASSPATH=$VIEW_HOME:$VIEW_HOME/lib/commons-cli-1.2.jar:$VIEW_HOME/lib/commons-cli-1.2.jar:$VIEW_HOME/lib/commons-lang3-3.1.jar:$VIEW_HOME/lib/gsp.jar:$VIEW_HOME/lib/jackson-annotations-2.2.3.jar:$VIEW_HOME/lib/jackson-core-2.2.3.jar:$VIEW_HOME/lib/jackson-databind-2.2.3.jar:$VIEW_HOME/lib/log4j-api-2.0-beta9.jar:lib/log4j-core-2.0-beta9.jar:$VIEW_HOME/lib/ojdbc7.jar:$VIEW_HOME/lib/oracle-ucp-11.2.jar
+
+export INPUT_QUERIES=/app/ids/dbcnew/viewanalyzer/Queries
+export OUTPUT_DIR=/app/ids/dbcnew/viewanalyzer/Output
+
+java -cp $ANALYSIS_CLASSPATH com.boettinger.analyzer.SqlViewsAnalyzerCli -i $INPUT_QUERIES -o $OUTPUT_DIR
