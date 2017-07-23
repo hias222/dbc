aa94f0da0f83f598a6c2ca7b15c01753f90bf3a2
bamboo <hias.fuchs@gmail.com>
2017-07-23 21:13:17 +0200

    ordered files

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# shell script
+
 export ANALYSIS_CLASSPATH=$VIEW_HOME:$VIEW_HOME/lib/commons-cli-1.2.jar:$VIEW_HOME/lib/commons-cli-1.2.jar:$VIEW_HOME/lib/commons-lang3-3.1.jar:$VIEW_HOME/lib/gsp.jar:$VIEW_HOME/lib/jackson-annotations-2.2.3.jar:$VIEW_HOME/lib/jackson-core-2.2.3.jar:$VIEW_HOME/lib/jackson-databind-2.2.3.jar:$VIEW_HOME/lib/log4j-api-2.0-beta9.jar:lib/log4j-core-2.0-beta9.jar:$VIEW_HOME/lib/ojdbc7.jar:$VIEW_HOME/lib/oracle-ucp-11.2.jar
 
 export INPUT_QUERIES=/app/ids/dbcnew/viewanalyzer/Queries
